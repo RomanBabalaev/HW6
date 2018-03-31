f91944eb204eb2eb216dcfa7869e7796c0f1870b
RomanBabalaev <36676728+RomanBabalaev@users.noreply.github.com>
2018-03-31 08:32:10 -0400

    Update image.php

--- image.php
+++ image.php
@@ -3,14 +3,14 @@ require_once 'ImageTransform.php';
 
 $img = new ImageTransform('bu_ob.jpg');
 
-/*//поворячиваем
-$img->rotate(50, 'buket_rotated .jpg');*/
+//поворячиваем
+$img->rotate(50, 'buket_rotated .jpg');
 
 //наносим ватермарк
 $img->setWatermark('buket.PNG', 0.75, 'center', 'buker_WM.jpg');
 
-/*//смена размерности
-$img->resizeProport(250,'buket_resized.jpg');*/
+//смена размерности
+$img->resizeProport(250,'buket_resized.jpg');
 
 
 
