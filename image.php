dc67230b38007d0433c3dea03ec0664a6beb8b7a
RomanBabalaev <36676728+RomanBabalaev@users.noreply.github.com>
2018-03-31 16:31:41 +0400

    Add files via upload

---
+++ image.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'ImageTransform.php';
+
+$img = new ImageTransform('bu_ob.jpg');
+
+/*//поворячиваем
+$img->rotate(50, 'buket_rotated .jpg');*/
+
+//наносим ватермарк
+$img->setWatermark('buket.PNG', 0.75, 'center', 'buker_WM.jpg');
+
+/*//смена размерности
+$img->resizeProport(250,'buket_resized.jpg');*/
+
+
+
